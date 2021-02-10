c6771a64c6774257a6f1246e9657bcfd4afbc9d4
ifamec <famousfame@qq.com>
2021-02-09 21:20:51 -0800

    message struct

---
+++ src/go_code/chapter18/ims/common/message/message.go
@@ -0,0 +1,25 @@
+package message
+
+// message type const
+
+const (
+	LoginMsgType    = "LoginMsg"
+	LoginRtnMsgType = "LoginRtnMsg"
+)
+
+type Message struct {
+	Type string `json:"type"` // message type
+	Data string `json:"data"` // data
+}
+
+// define two message
+type LoginMsg struct {
+	UserId   int    `json:"userId"`   // user id
+	UserPwd  string `json:"userPwd"`  // user password
+	UserName string `json:"userName"` // user name
+}
+
+type LoginRtnMsg struct {
+	Code  int    `json:"code"`  // 500 not registered, 200 success
+	Error string `json:"error"` // Error message
+}
