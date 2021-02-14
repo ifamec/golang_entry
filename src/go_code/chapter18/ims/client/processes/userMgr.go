d28b1739fd50b68c54584420b79c0dd1e6c01502
ifamec <famousfame@qq.com>
2021-02-13 17:01:57 -0800

    maintain online user list on client side

---
+++ src/go_code/chapter18/ims/client/processes/userMgr.go
@@ -0,0 +1,31 @@
+package processes
+
+import (
+	"fmt"
+	"go_code/chapter18/ims/common/message"
+)
+
+// map
+var onlineUsers map[int]*message.User = make(map[int]*message.User, 10)
+
+// handle return message
+func updateUserStatus(notifyUserStatusMsg *message.NotifyUserStatusMsg) {
+
+	// if user exist
+	user, isExist := onlineUsers[notifyUserStatusMsg.UserId]
+	if !isExist {
+		user = &message.User{
+			UserId:     notifyUserStatusMsg.UserId,
+		}
+	}
+	user.UserStatus = notifyUserStatusMsg.Status
+	onlineUsers[notifyUserStatusMsg.UserId] = user
+	printOnlineUser()
+}
+
+func printOnlineUser()  {
+	fmt.Println("Current Online User List:")
+	for id, _ := range onlineUsers {
+		fmt.Println("User Id:", id)
+	}
+}
